2838ba0e50a571870a7959c48b89bdf6abe24290
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-07-31 12:00:20 -0400

    Create members.js
    Moved json out of index.js

---
+++ members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1, 
+        name: 'Jon Doe', 
+        email: 'jd@gmail.com', 
+        status: 'active'
+    },
+    {
+        id: 2, 
+        name: 'Bob Williams', 
+        email: 'bw@gmail.com', 
+        status: 'inactive'
+    }, 
+    {
+        id: 3, 
+        name: 'Shannon Jackson', 
+        email: 'sj@gmail.com', 
+        status: 'active'
+    }
+];
+
+module.exports = members;
